2773b1c5e640dd57fc1e4686d71553ecc729b862
zhaoxiaohui <49872684+xiaohui1227@users.noreply.github.com>
2021-02-13 17:05:49 +0800

    Update test.java

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -3,7 +3,8 @@ public class test {
         System.out.println("a");
         System.out.println("b");
         System.out.println("c");
-        System.out.println("c");
-        System.out.println("c");
+        System.out.println("d");
+        System.out.println("e");
+        System.out.println("f");
     }
 }
